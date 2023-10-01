179a0ee993a5cda432ed0a52d99341944ee2f2d6
clark.android@gmail.com <clark.android@gmail.com>
2023-10-01 23:04:43 +0800

    add animation of enemy

--- MyGame/assets/script/canvas.ts
+++ MyGame/assets/script/canvas.ts
@@ -1,21 +1,51 @@
-const {ccclass, property} = cc._decorator;
+const { ccclass, property } = cc._decorator;
 
 @ccclass
 export default class Canvas extends cc.Component {
 
     @property(cc.Prefab)
-    enemyPre:cc.Prefab = null
+    enemyPinkPre: cc.Prefab = null
+    @property(cc.Prefab)
+    enemyBeigePre: cc.Prefab = null
+    @property(cc.Prefab)
+    enemyGreenPre: cc.Prefab = null
+    @property(cc.Prefab)
+    enemyBluePre: cc.Prefab = null
+    @property(cc.Prefab)
+    enemyYellowPre: cc.Prefab = null
 
     // LIFE-CYCLE CALLBACKS:
 
     // onLoad () {}
 
-    start () {
-        this.schedule(()=>{
-            let enemy = cc.instantiate(this.enemyPre)
-            enemy.setParent(cc.director.getScene())
+    start() {
+        this.schedule(() => {
+            let count = Math.trunc(Math.random() * 10 % 5)
+            cc.log("count: " + count)
+            switch (count) {
+                case 0:
+                    let enemyPink = cc.instantiate(this.enemyPinkPre)
+                    enemyPink.setParent(cc.director.getScene())
+                    break;
+                case 1:
+                    let enemyBeige = cc.instantiate(this.enemyBeigePre)
+                    enemyBeige.setParent(cc.director.getScene())
+                    break;
+                case 2:
+                    let enemyGreen = cc.instantiate(this.enemyGreenPre)
+                    enemyGreen.setParent(cc.director.getScene())
+                    break;
+                case 3:
+                    let enemyBlue = cc.instantiate(this.enemyBluePre)
+                    enemyBlue.setParent(cc.director.getScene())
+                    break;
+                case 4:
+                    let enemyYellow = cc.instantiate(this.enemyYellowPre)
+                    enemyYellow.setParent(cc.director.getScene())
+                    break;
+            }
         }, 3)
-        
+
     }
 
     // update (dt) {}
---
+++ MyGame/assets/script/enemy_blue_script.ts
@@ -0,0 +1,46 @@
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class EnemyBlue extends cc.Component {
+
+
+    scrWidth: number
+    scrHeigh: number
+
+    @property
+    speed: number = 50
+
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad() {
+        let cm = cc.director.getCollisionManager()
+        cm.enabled = true
+        // cm.enabledDebugDraw = true
+
+        this.scrWidth = cc.view.getCanvasSize().width
+        this.scrHeigh = cc.view.getCanvasSize().height
+    }
+
+    start() {
+        this.node.x = this.scrWidth + this.node.width
+        this.node.y = (Math.trunc((Math.random() * 10) % 6) + 2) * 64 + 32
+        cc.log("this.node.y: " + this.node.y)
+    }
+
+    update(dt) {
+        this.node.x -= this.speed * dt
+        if (this.node.x - this.node.width / 2 < 0) {
+            this.node.removeFromParent(true)
+            this.destroy()
+        }
+    }
+
+    onCollisionEnter(other: cc.Collider, self: cc.Collider){
+        if(other.tag == 0){
+            this.node.removeFromParent(true)
+            this.destroy()
+            other.node.removeFromParent(true)
+            other.destroy()
+        }
+    }
+}
